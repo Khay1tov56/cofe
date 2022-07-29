86bd1a34b6318d2a70afca44fe5befce84cfa24e
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-07-29 17:27:06 +0300

    Add btn

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-var elModalOpenButton = document.querySelector(".site-header__menu-btn");
-var elModal = document.querySelector(".site-header__inner");
+// var elBtnOpen = document.querySelector(".site-header__menu-btn");
+// var elNav = document.querySelector(".sitenav");
+// var elList = document.querySelector(".sitenav__list");
 
-elModalOpenButton.addEventListener("click", function(){
-  elModal.classList.add("site-header__sitenav");
-})
+// elBtnOpen.addEventListener("click", () => {
+//   elNav.classList.toggle("navBlock");
+//   elList.classList.toggle("active");
+// })
 
+var elHeader = document.querySelector(".site-header")
+var elBars = document.querySelector(".site-header__menu-btn")
 
-var elModalCloseButton = document.querySelector(".site-header__menu-btn");
-
-elModalCloseButton.addEventListener("click" , function (){
-  elModal.classList.remove("site-header__sitenav");
+elBars.addEventListener("click", ()=> {
+    elHeader.classList.toggle("site-nav-on")
 })
