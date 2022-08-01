5818910fe3c5ac7660b9080c73cfabf648d20614
NurilloOkeng5670 <khayitov5670@gmail.com>
2022-08-01 21:38:50 +0300

    Add subscribe

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
-// var elBtnOpen = document.querySelector(".site-header__menu-btn");
-// var elNav = document.querySelector(".sitenav");
-// var elList = document.querySelector(".sitenav__list");
-
-// elBtnOpen.addEventListener("click", () => {
-//   elNav.classList.toggle("navBlock");
-//   elList.classList.toggle("active");
-// })
 
 var elHeader = document.querySelector(".site-header")
 var elBars = document.querySelector(".site-header__menu-btn")
 
 elBars.addEventListener("click", ()=> {
     elHeader.classList.toggle("site-nav-on")
+})
+
+var elModal = document.querySelector(".modal");
+var elOpenBtn = document.querySelector(".bean__btn")
+var elCloseBtn = document.querySelector(".modal__btn")
+var elModals = document.querySelector(".modal__btns")
+
+elOpenBtn.addEventListener("click", ()=> {
+    elModal.classList.add("modal__list")
+});
+
+elCloseBtn.addEventListener("click", ()=> {
+    elModal.classList.remove("modal__list")
+});
+
+elModals.addEventListener("click", ()=> {
+    elModal.classList.remove("modal__list")
 })
